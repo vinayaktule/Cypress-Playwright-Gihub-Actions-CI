56d1b6cb21f4f613f56a773f8ba46a74be8565f0
vinayaktule <vinayaktule@gmail.com>
2024-06-30 10:13:01 +0530

    allure-integration with cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,18 @@
 const { defineConfig } = require("cypress");
-
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) { },
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
     watchForFileChanges: false,
     defaultCommandTimeout: 10000,
     screenshotOnRunFailure: true,
     specPattern: "cypress/e2e//*.cy.{js,jsx,ts,ysx}",
+    env: {
+      allureReuseAfterSpec: true,
+      allure: false
+    }
   },
 });
